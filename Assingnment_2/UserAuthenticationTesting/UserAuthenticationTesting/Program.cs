4f6f9e9d64164c8febcf340a1267675caf63a94e
unknown <vasanthgamer2000@gmail.com>
2023-09-12 15:50:18 +0530

    UserAuthenticationTesting

---
+++ Assingnment_2/UserAuthenticationTesting/UserAuthenticationTesting/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAuthenticationTesting
+{
+    public class UserAuthenticator
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public UserAuthenticator()
+        {
+            Username = null;
+            Password = null;
+        }
+
+        public bool RegisterUser(string username, string password)
+        {
+            if(Username == null) 
+            {
+                Username = username;
+                Password = password;
+
+                return true;
+            }
+            
+            return false;
+                   
+        }
+
+        public bool LoginUser(string username, string password)
+        {
+            if(Username == username && Password == password) 
+            {
+                return true;
+            }
+           
+            return false;
+            
+        }
+
+        public bool ResetPassword(string username, string password)
+        {
+            if(Username == username)
+            {
+                Password = password;
+                return true;
+            }
+            
+            return false;
+            
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Assingnment_2/UserAuthenticationTesting/UserAuthenticationTesting/UserAuthenticatorTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAuthenticationTesting
+{
+    [TestFixture]
+    public class UserAuthenticatorTest
+    {
+        public UserAuthenticator authenticator;
+
+        [SetUp]
+        public void Setup()
+        {
+            authenticator = new UserAuthenticator();
+        }
+
+        [Test]
+        public void TestUserRegistration()
+        {
+            Assert.IsTrue(authenticator.RegisterUser("user1", "password1"));
+            Assert.IsFalse(authenticator.RegisterUser("user2", "password2")); // User already registered
+        }
+
+        [Test]
+        public void TestUserLogin()
+        {
+            authenticator.RegisterUser("user3", "password3");
+
+            Assert.IsTrue(authenticator.LoginUser("user3", "password3"));
+            Assert.IsFalse(authenticator.LoginUser("user3", "wrongpassword")); // Invalid password
+            Assert.IsFalse(authenticator.LoginUser("nonexistentuser", "password")); // User does not exist
+        }
+
+        [Test]
+        public void TestPasswordReset()
+        {
+            authenticator.RegisterUser("user4", "password4");
+
+            Assert.IsTrue(authenticator.ResetPassword("user4", "newpassword"));
+            Assert.IsFalse(authenticator.ResetPassword("nonexistentuser", "newpassword")); // User does not exist
+        }
+    }
+}
