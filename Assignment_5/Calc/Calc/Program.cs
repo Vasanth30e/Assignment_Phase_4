e0f9ef648c99b70a2f67a18b5783b5827e0b1df4
unknown <vasanthgamer2000@gmail.com>
2023-09-15 16:17:58 +0530

    Changes has been committed

--- Assignment_5/Calc/Calc/Program.cs
+++ Assignment_5/Calc/Calc/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calc;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Arithmatic arithmatic = new Arithmatic();
+        Console.WriteLine("Arithmetic Operations");
+        Console.WriteLine("Add two numbers");
+
+        Console.WriteLine("Result: " + arithmatic.Add(12, 12));
+
+        Console.WriteLine("Arithmetic Operations");
+        Console.WriteLine("Subtract two numbers");
+
+        Console.WriteLine("Result: " + arithmatic.Sub(12, 12));
+
+        Console.WriteLine("Arithmetic Operations");
+        Console.WriteLine("Multiply two numbers");
+
+        Console.WriteLine("Result: " + arithmatic.Mul(12, 12));
+
+        Console.WriteLine("Arithmetic Operations");
+        Console.WriteLine("Divide two numbers");
+
+        Console.WriteLine("Result: " + arithmatic.Div(12, 12));
+    }
+}
